823b1284ce75b04bd97133969f2a87e365b6e0a1
Odyseus <venusyadonis@gmail.com>
2022-06-06 13:58:15 -0300

    shell_utils.py: added type annotations and docstrings update

--- shell_utils.py
+++ shell_utils.py
@@ -4,7 +4,7 @@
 from shutil import get_terminal_size
 
 
-def get_cli_header(name, char="#"):
+def get_cli_header(name: str, char: str = "#") -> str:
     """Get a "decorated header".
 
     Get a "decorated header" to display at the beginning of the CLI execution (or whenever it is
@@ -22,10 +22,10 @@ def get_cli_header(name, char="#"):
     str
         The actual "header".
     """
-    term_length = get_terminal_size((80, 24))[0] or 80
-    sep = get_cli_separator(char)
-    sub_sep = "%s" % (int((term_length - (len(name) + 2)) / 2) * char)
-    mid = "%s %s %s" % (sub_sep, name, sub_sep)
+    term_length: int = get_terminal_size((80, 24))[0] or 80
+    sep: str = get_cli_separator(char)
+    sub_sep: str = "%s" % (int((term_length - (len(name) + 2)) / 2) * char)
+    mid: str = "%s %s %s" % (sub_sep, name, sub_sep)
 
     while True:
         if len(mid) < term_length:
@@ -35,14 +35,14 @@ def get_cli_header(name, char="#"):
         if len(mid) >= term_length:
             break
 
-    header = sep + "\n"
+    header: str = sep + "\n"
     header += mid[:term_length] + "\n"
     header += sep
 
     return header
 
 
-def get_cli_separator(char="#"):
+def get_cli_separator(char: str = "#") -> str:
     """Get a "decorated separator".
 
     Get a "decorated separator" to display whenever it is needed.
@@ -57,7 +57,7 @@ def get_cli_separator(char="#"):
     str
         The actual "separator".
     """
-    term_length = get_terminal_size((80, 24))[0] or 80
+    term_length: int = get_terminal_size((80, 24))[0] or 80
     return "%s" % (term_length * char)
 
 
