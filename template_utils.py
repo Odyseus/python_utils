882883fb1219411862fb8df9e6469b7d68190b04
Odyseus <venusyadonis@gmail.com>
2022-06-06 13:46:56 -0300

    template_utils.py
    - Adaptations to changes made to the `ansi_colors.py` module.
    - Added type annotations and docstrings update.

--- template_utils.py
+++ template_utils.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 """Utilities to generate data from templates.
-
-
-Attributes
-----------
-BASH_COMPLETION_LOADER_CONTENT : str
-    Bash completions loader script.
-bash_completions_step1 : str
-    Bash completions creation message. Step 1.
-bash_completions_step2 : str
-    Bash completions creation message. Step 2.
 """
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from . import logging_system
+
 import os
 import sys
 
@@ -19,10 +16,10 @@ from . import file_utils
 from . import misc_utils
 from . import prompts
 from . import shell_utils
-from .ansi_colors import Ansi
+from .ansi_colors import colorize
 
 
-BASH_COMPLETION_LOADER_CONTENT = """
+_bash_completion_loader_content: str = """
 if [[ -d "$HOME/.bash_completion.d" ]]; then
     for bcfile in "$HOME/.bash_completion.d"/*; do
         . $bcfile
@@ -31,7 +28,7 @@ fi
 """
 
 
-bash_completions_step1 = """
+_bash_completions_step_1: str = """
 **Bash completions creation. Step 1.**
 
 The file {0}
@@ -39,7 +36,7 @@ will be created.
 """
 
 
-bash_completions_step2 = """
+_bash_completions_step_2: str = """
 **Bash completions creation. Step 2.**
 
 The file **{0}/.bash_completion** will be created if it doesn't exists.
@@ -55,19 +52,22 @@ The following is the content that will be appended to the
 """
 
 
-def system_executable_generation(exec_name="",
-                                 exec_destination="",
-                                 app_root_folder="",
-                                 sys_exec_template_path="",
-                                 bash_completions_template_path="",
-                                 do_completions=True,
-                                 logger=None,
-                                 bash_comp_step_one=True,
-                                 bash_comp_step_two=True,
-                                 prompt_for_name=True,
-                                 prompt_for_path=True,
-                                 bash_comp_step_one_confirmed=False,
-                                 confirm_overwrite=True):
+def system_executable_generation(
+    exec_name: str = "",
+    exec_destination: str = "",
+    app_root_folder: str = "",
+    sys_exec_template_path: str = "",
+    bash_completions_template_path: str = "",
+    do_completions: bool = True,
+    logger: logging_system.Logger | None = None,
+    bash_comp_step_one: bool = True,
+    bash_comp_step_two: bool = True,
+    prompt_for_name: bool = True,
+    prompt_for_path: bool = True,
+    bash_comp_step_one_confirmed: bool = False,
+    confirm_overwrite: bool = True,
+    inject_interpreter: bool = False,
+) -> None:
     """Generate a system executable.
 
     Parameters
@@ -85,7 +85,7 @@ def system_executable_generation(exec_name="",
         Bash completions file.
     do_completions : bool, optional
         Perform the bash completions installation.
-    logger : LogSystem, optional
+    logger : logging_system.Logger | None, optional
         The logger.
     bash_comp_step_one : bool, optional
         If True, continue with execution. If False, halt execution.
@@ -122,76 +122,98 @@ def system_executable_generation(exec_name="",
         logger.error("**<bash_completions_template_path> parameter should be a real file.**")
         raise SystemExit()
 
-    user_home = os.path.expanduser("~")
+    user_home: str = os.path.expanduser("~")
 
-    d = {
+    d: dict = {
         "name": exec_name,
-        "sys_exec_path": exec_destination or os.path.join(user_home, ".local", "bin")
+        "sys_exec_path": exec_destination or os.path.join(user_home, ".local", "bin"),
     }
 
     if prompt_for_name:
-        print(Ansi.LIGHT_MAGENTA("Set an executable file name or press Enter to use default"))
+        print(colorize("Set an executable file name or press Enter to use default", "lightmagenta"))
         prompts.do_prompt(d, "name", "Enter a file name", d["name"])
 
     if prompt_for_path:
-        print(Ansi.LIGHT_MAGENTA("Set full path to store executable file or press Enter to use default"))
+        print(
+            colorize(
+                "Set full path to store executable file or press Enter to use default",
+                "lightmagenta",
+            )
+        )
         prompts.do_prompt(d, "sys_exec_path", "Enter absolute path", d["sys_exec_path"])
 
     d["sys_exec_path"] = os.path.expanduser(d["sys_exec_path"])
     d["sys_exec_path"] = os.path.expandvars(d["sys_exec_path"])
 
-    destination = os.path.join(d["sys_exec_path"], d["name"])
+    destination: str = os.path.join(d["sys_exec_path"], d["name"])
 
     if not os.path.exists(d["sys_exec_path"]):
-        print(Ansi.LIGHT_YELLOW("Path doesn't exists and needs to be created"))
+        print(colorize("Path doesn't exists and needs to be created", "lightyellow"))
 
         if prompts.confirm(prompt="Proceed?", response=False):
             os.makedirs(d["sys_exec_path"], exist_ok=True)
     elif not file_utils.is_real_dir(d["sys_exec_path"]):
-        print(Ansi.LIGHT_YELLOW("Chosen path isn't a directory. Aborted!!!"))
+        print(colorize("Chosen path isn't a directory. Aborted!!!", "lightyellow"))
         raise SystemExit()
 
     logger.info("**Generating system executable...**")
 
-    generate_from_template(sys_exec_template_path, destination, options={
-        "replacements": [
-            ("{full_path_to_app_folder}", app_root_folder)
-        ],
-        "set_executable": True
-    }, logger=logger, confirm_overwrite=confirm_overwrite)
+    replacements: list[tuple[str, str]] = [
+        ("{full_path_to_app_folder}", app_root_folder),
+        ("{interpreter}", sys.executable if inject_interpreter else ""),
+    ]
+
+    generate_from_template(
+        sys_exec_template_path,
+        destination,
+        options={
+            "replacements": replacements,
+            "set_executable": True,
+        },
+        logger=logger,
+        confirm_overwrite=confirm_overwrite,
+    )
 
     if not do_completions:
         return
 
-    bash_completions_file_destination = os.path.join(
-        user_home, ".bash_completion.d", d["name"] + ".completion.bash")
-    bash_completions_loader = os.path.join(user_home, ".bash_completion")
+    bash_completions_file_destination: str = os.path.join(
+        user_home, ".bash_completion.d", d["name"] + ".completion.bash"
+    )
+    bash_completions_loader: str = os.path.join(user_home, ".bash_completion")
 
     if not bash_comp_step_one:
         return
 
-    print(Ansi.LIGHT_MAGENTA(bash_completions_step1.format(bash_completions_file_destination)))
+    print(
+        colorize(_bash_completions_step_1.format(bash_completions_file_destination), "lightmagenta")
+    )
 
     if bash_comp_step_one_confirmed or prompts.confirm(prompt="Proceed?", response=False):
         logger.info("**Generating bash completions file...**")
 
-        generate_from_template(bash_completions_template_path, bash_completions_file_destination,
-                               options={
-                                   "replacements": [
-                                       ("{current_date}", misc_utils.get_date_time("function_name")),
-                                       ("{full_path_to_app_folder}", app_root_folder),
-                                       ("{executable_name}", d["name"])
-                                   ],
-                                   "set_executable": False
-                               }, logger=logger, confirm_overwrite=confirm_overwrite)
+        generate_from_template(
+            bash_completions_template_path,
+            bash_completions_file_destination,
+            options={
+                "replacements": [
+                    ("{current_date}", misc_utils.get_date_time("function_name")),
+                    ("{full_path_to_app_folder}", app_root_folder),
+                    ("{executable_name}", d["name"]),
+                ],
+                "set_executable": False,
+            },
+            logger=logger,
+            confirm_overwrite=confirm_overwrite,
+        )
 
     if not bash_comp_step_two:
         return
 
-    print(Ansi.LIGHT_MAGENTA(bash_completions_step2.format(os.path.expanduser("~"))))
-    print(Ansi.DEFAULT(shell_utils.get_cli_separator()))
-    print(Ansi.DEFAULT(BASH_COMPLETION_LOADER_CONTENT))
-    print(Ansi.DEFAULT(shell_utils.get_cli_separator()))
+    print(colorize(_bash_completions_step_2.format(os.path.expanduser("~")), "lightmagenta"))
+    print(colorize(shell_utils.get_cli_separator()))
+    print(colorize(_bash_completion_loader_content))
+    print(colorize(shell_utils.get_cli_separator()))
 
     if prompts.confirm(prompt="Proceed?", response=False):
         logger.info("**Attempting to set up Bash completions loader...**")
@@ -201,16 +223,18 @@ def system_executable_generation(exec_name="",
             # load the content of this directory as bash completions.
             with open(bash_completions_loader, "a+", encoding="UTF-8") as file:
                 file.seek(0)
-                found = any("/.bash_completion.d/" in line for line in file)
+                found: bool = any("/.bash_completion.d/" in line for line in file)
 
                 if found:
-                    logger.info("**The <%s/.bash_completion.d> directory seems to be set up.**" %
-                                user_home)
-                    logger.info("**Check the <%s> file content just in case.**" %
-                                bash_completions_loader)
+                    logger.info(
+                        "**The <%s/.bash_completion.d> directory seems to be set up.**" % user_home
+                    )
+                    logger.info(
+                        "**Check the <%s> file content just in case.**" % bash_completions_loader
+                    )
                     sys.exit(0)
                 else:
-                    file.write(BASH_COMPLETION_LOADER_CONTENT)
+                    file.write(_bash_completion_loader_content)
                     logger.info("**Bash completions loader set up.**")
 
             sys.exit(0)
@@ -218,7 +242,12 @@ def system_executable_generation(exec_name="",
             logger.error(err)
 
 
-def do_template_copy(source, destination, options={}, logger=None):
+def do_template_copy(
+    source: str,
+    destination: str,
+    options: dict = {},
+    logger: logging_system.Logger | None = None,
+) -> None:
     """Do the actual copy of template files.
 
     Parameters
@@ -229,7 +258,7 @@ def do_template_copy(source, destination, options={}, logger=None):
         Full file path destination.
     options : dict, optional
         A dictionary of options.
-    logger : LogSystem
+    logger : logging_system.Logger | None, optional
         The logger.
 
     Raises
@@ -238,7 +267,7 @@ def do_template_copy(source, destination, options={}, logger=None):
         Halt execution if any error is found.
     """
     with open(source, "r", encoding="UTF-8") as template_file:
-        template_data = template_file.read()
+        template_data: str = template_file.read()
 
     try:
         if options.get("replacements", False):
@@ -254,7 +283,13 @@ def do_template_copy(source, destination, options={}, logger=None):
         os.chmod(destination, 0o777)
 
 
-def generate_from_template(source, destination, options={}, logger=None, confirm_overwrite=True):
+def generate_from_template(
+    source: str,
+    destination: str,
+    options: dict = {},
+    logger: logging_system.Logger | None = None,
+    confirm_overwrite: bool = True,
+) -> None:
     """Generate a file from a template.
 
     Parameters
@@ -271,7 +306,7 @@ def generate_from_template(source, destination, options={}, logger=None, confirm
         - **set_executable**: A bool used to determine if the file created by \
         :any:`do_template_copy` should be made executable.
 
-    logger : LogSystem
+    logger : logging_system.Logger | None, optional
         The logger.
     confirm_overwrite : bool, optional
         If True, ask to overwrite files. If False, proceed without confirmation.
@@ -297,7 +332,7 @@ def generate_from_template(source, destination, options={}, logger=None, confirm
             else:
                 raise SystemExit()
         else:
-            dirname = os.path.dirname(destination)
+            dirname: str = os.path.dirname(destination)
 
             if os.path.exists(dirname) and (os.path.isfile(dirname) or os.path.islink(dirname)):
                 logger.error("**Destination <%s> should be a directory!!!**" % dirname)
