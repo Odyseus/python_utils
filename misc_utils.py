f28b8572d14a0781a8a5f54b679ba1d7e883bf8b
Odyseus <venusyadonis@gmail.com>
2022-06-06 14:00:19 -0300

    misc_utils.py: added type annotations and docstrings update

--- misc_utils.py
+++ misc_utils.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
-"""Miscellaneous utility functions/properties.
+"""Miscellaneous utility functions.
 """
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .logging_system import Logger
+    from datetime import timedelta
+    from typing import Any
+
 from datetime import datetime
 
 
-def get_system_tempdir():
+def get_system_tempdir() -> str:
     """Get system's temporary directory.
 
     Returns
@@ -17,7 +26,7 @@ def get_system_tempdir():
     return tempfile.gettempdir()
 
 
-def get_date_time(type="date"):
+def get_date_time(type: str = "date") -> str:
     """Get date time.
 
     Parameters
@@ -40,7 +49,7 @@ def get_date_time(type="date"):
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 
 
-def micro_to_milli(date):
+def micro_to_milli(date: str) -> str:
     """Microseconds to milliseconds.
 
     Convert a date string from using microseconds to use milliseconds.
@@ -58,7 +67,7 @@ def micro_to_milli(date):
     return date[:-6] + str("{0:03d}".format(int(int(date[-6:]) / 1000)))
 
 
-def get_time_diff(s, e):
+def get_time_diff(s: str, e: str) -> str:
     """Get time difference.
 
     Parameters
@@ -73,19 +82,25 @@ def get_time_diff(s, e):
     str
         The difference in hours, minutes, seconds, and milliseconds between two dates.
     """
-    start = datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f")
-    ends = datetime.strptime(e, "%Y-%m-%d %H:%M:%S.%f")
+    start: datetime = datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f")
+    ends: datetime = datetime.strptime(e, "%Y-%m-%d %H:%M:%S.%f")
 
-    diff = ends - start
+    diff: timedelta = ends - start
 
     m, s = divmod(diff.seconds, 60)
     h, m = divmod(m, 60)
-    ms = diff.microseconds / 1000
+    ms: float = diff.microseconds / 1000
 
     return "%d hr/s, %d min/s, %d sec/s, %d msec/s" % (h, m, s, ms)
 
 
-def merge_dict(first, second, logger=None, extend_lists=True, append_to_lists=True):
+def merge_dict(
+    first: Any,
+    second: Any,
+    logger: Logger = None,
+    extend_lists: bool = True,
+    append_to_lists: bool = True,
+) -> Any:
     """Merges **second** dictionary into **first** dictionary and return merged result.
 
     It *deep merges* keys of type :any:`dict` and :any:`list`.
@@ -93,11 +108,11 @@ def merge_dict(first, second, logger=None, extend_lists=True, append_to_lists=Tr
 
     Parameters
     ----------
-    first : dict
+    first : Any
         A dictionary to which to merge a second dictinary.
-    second : dict
+    second : Any
         A dictionary to merge into another dictionary.
-    logger : LogSystem
+    logger : Logger, optional
         The logger.
     extend_lists : bool, optional
         When dealing with lists, extend the ``first`` list one with the ``second`` one.
@@ -106,10 +121,10 @@ def merge_dict(first, second, logger=None, extend_lists=True, append_to_lists=Tr
 
     Returns
     -------
-    dict
+    Any
         A merged dictionary.
     """
-    key = None
+    key: str = None
     try:
         if isinstance(first, list):
             # Lists can be only appended.
@@ -127,10 +142,13 @@ def merge_dict(first, second, logger=None, extend_lists=True, append_to_lists=Tr
             if isinstance(second, dict):
                 for key in second:
                     if key in first:
-                        first[key] = merge_dict(first[key], second[key],
-                                                extend_lists=extend_lists,
-                                                append_to_lists=append_to_lists,
-                                                logger=logger)
+                        first[key] = merge_dict(
+                            first[key],
+                            second[key],
+                            extend_lists=extend_lists,
+                            append_to_lists=append_to_lists,
+                            logger=logger,
+                        )
                     else:
                         first[key] = second[key]
             else:
@@ -138,8 +156,9 @@ def merge_dict(first, second, logger=None, extend_lists=True, append_to_lists=Tr
         else:
             first = second
     except TypeError as err:
-        logger.error('**TypeError "%s" in key "%s" when merging "%s" into "%s"**' %
-                     (err, key, second, first))
+        logger.error(
+            '**TypeError "%s" in key "%s" when merging "%s" into "%s"**' % (err, key, second, first)
+        )
 
     return first
 
