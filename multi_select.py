f3f9a5490ac454ab01562e1464e54f5c0740aeea
Odyseus <venusyadonis@gmail.com>
2022-06-06 13:59:57 -0300

    multi_select.py: added type annotations and docstrings update

--- multi_select.py
+++ multi_select.py
@@ -2,6 +2,7 @@
 
 """Multi selection menu creator.
 """
+from __future__ import annotations
 
 import curses
 
@@ -10,14 +11,14 @@ from shutil import get_terminal_size
 from . import exceptions
 
 
-class MultiSelect():
+class MultiSelect:
     """Allows you to select from a list with curses.
 
     Attributes
     ----------
     aborted : bool
         Menu selection aborted.
-    all_menu_items : list
+    all_menu_items : list[dict]
         The menu items that constitute the multi select menu.
     arrow : str
         The character/s used to *point* the menu item that can be selected.
@@ -39,11 +40,11 @@ class MultiSelect():
         The amount of selected menu items.
     selected : int
         The index of the menu item on the current cursor position.
-    stdscr : curses.initscr
+    stdscr : curses.window | None
         Initialize the library.
     title : str
         A title to use on the menu.
-    win : curses.newwin
+    win : curses.window | None
         A new window object.
     window_height : int
         The height of the terminal window.
@@ -73,14 +74,21 @@ class MultiSelect():
             print(opts)
     """
 
-    def __init__(self, menu_items=[], title="", arrow="==>",
-                 footer="Space = toggle ─ Enter = accept ─ q = cancel",
-                 more="...", char_selected="[X]", char_empty="[ ]"):
+    def __init__(
+        self,
+        menu_items: list[str] = [],
+        title: str = "",
+        arrow: str = "==>",
+        footer: str = "Space = toggle ─ Enter = accept ─ q = cancel",
+        more: str = "...",
+        char_selected: str = "[X]",
+        char_empty: str = "[ ]",
+    ) -> None:
         """Initialization.
 
         Parameters
         ----------
-        menu_items : list
+        menu_items : list[str], optional
             The data that will be used to create the menu items.
         title : str, optional
             A title to use on the menu.
@@ -95,83 +103,72 @@ class MultiSelect():
         char_empty : str, optional
             The character/s used to represent a non selected menu item.
         """
-        self.title = title
-        self.arrow = arrow
-        self.footer = footer
-        self.more = more
-        self.char_selected = char_selected
-        self.char_empty = char_empty
+        self.title: str = title
+        self.arrow: str = arrow
+        self.footer: str = footer
+        self.more: str = more
+        self.char_selected: str = char_selected
+        self.char_empty: str = char_empty
 
-        self.all_menu_items = []
-        self.win = None
-        self.stdscr = None
-        self.cursor = 0
-        self.offset = 0
-        self.selected = 0
-        self.selcount = 0
-        self.aborted = False
-        self.window_height = (get_terminal_size((80, 24))[1] or 24) - 5
-        self.window_width = (get_terminal_size((80, 24))[0] or 80)
-        self.length = 0
+        self.all_menu_items: list[dict] = []
+        self.win: curses.window | None = None
+        self.stdscr: curses.window | None = None
+        self.cursor: int = 0
+        self.offset: int = 0
+        self.selected: int = 0
+        self.selcount: int = 0
+        self.aborted: bool = False
+        self.window_height: int = (get_terminal_size((80, 24))[1] or 24) - 5
+        self.window_width: int = get_terminal_size((80, 24))[0] or 80
+        self.length: int = 0
 
         for item in menu_items:
-            self.all_menu_items.append({
-                "label": item,
-                "selected": False
-            })
+            self.all_menu_items.append({"label": item, "selected": False})
             self.length = len(self.all_menu_items)
 
         self.curses_start()
         curses.wrapper(self.curses_loop)
         self.curses_stop()
 
-    def curses_start(self):
-        """curses start.
-        """
+    def curses_start(self) -> None:
+        """curses start."""
         self.stdscr = curses.initscr()
 
         curses.noecho()
         curses.cbreak()
         curses.curs_set(0)
-        self.win = curses.newwin(
-            5 + self.window_height,
-            self.window_width,
-            0,
-            0
-        )
+        self.win = curses.newwin(5 + self.window_height, self.window_width, 0, 0)
 
-    def curses_stop(self):
-        """curses stop.
-        """
+    def curses_stop(self) -> None:
+        """curses stop."""
         curses.nocbreak()
-        self.stdscr.keypad(0)
+        self.stdscr.keypad(False)
         curses.echo()
         curses.endwin()
 
-    def getSelected(self):
+    def getSelected(self) -> list[str]:
         """Get selected.
 
         Returns
         -------
-        list
+        list[str]
             The labels of the selected menu items.
         """
         if self.aborted:
-            return False
+            return []
 
-        selected_items = [x for x in self.all_menu_items if x["selected"]]
-        selected_items_labels = [x["label"] for x in selected_items]
+        selected_items: list[dict] = [x for x in self.all_menu_items if x["selected"]]
+        selected_items_labels: list[str] = [x["label"] for x in selected_items]
         return selected_items_labels
 
-    def redraw(self):
-        """Redraw.
-        """
+    def redraw(self) -> None:
+        """Redraw."""
         self.win.clear()
         self.win.box(0, 0)
         self.win.addstr(self.window_height + 4, 5, " " + self.footer + " ", curses.A_BOLD)
 
         position = 0
-        items_range = self.all_menu_items[self.offset:self.offset + self.window_height + 1]
+        items_range = self.all_menu_items[self.offset: self.offset + self.window_height + 1]
 
         for option in items_range:
             if option["selected"]:
@@ -192,24 +189,23 @@ class MultiSelect():
 
         self.win.addstr(0, 5, " " + self.title + " ", curses.A_BOLD)
         self.win.addstr(
-            0, self.window_width - 8,
+            0,
+            self.window_width - 8,
             " " + str(self.selcount) + "/" + str(self.length) + " ",
-            curses.A_BOLD
+            curses.A_BOLD,
         )
         self.win.addstr(self.cursor + 2, 1, self.arrow, curses.A_BOLD)
         self.win.refresh()
 
-    def check_cursor_up(self):
-        """Check cursor up.
-        """
+    def check_cursor_up(self) -> None:
+        """Check cursor up."""
         if self.cursor < 0:
             self.cursor = 0
             if self.offset > 0:
                 self.offset = self.offset - 1
 
-    def check_cursor_down(self):
-        """Check cursor down.
-        """
+    def check_cursor_down(self) -> None:
+        """Check cursor down."""
         if self.cursor >= self.length:
             self.cursor = self.cursor - 1
 
@@ -220,7 +216,7 @@ class MultiSelect():
             if self.offset + self.cursor >= self.length:
                 self.offset = self.offset - 1
 
-    def curses_loop(self, stdscr):
+    def curses_loop(self, stdscr) -> None:
         """Curses loop.
 
         Parameters
@@ -245,9 +241,10 @@ class MultiSelect():
                     self.cursor = self.cursor - 1
                 elif c == curses.KEY_DOWN:
                     self.cursor = self.cursor + 1
-                elif c == ord(' '):
-                    self.all_menu_items[self.selected]["selected"] = \
-                        not self.all_menu_items[self.selected]["selected"]
+                elif c == ord(" "):
+                    self.all_menu_items[self.selected]["selected"] = not self.all_menu_items[
+                        self.selected
+                    ]["selected"]
                 elif c == 10:
                     break
 
